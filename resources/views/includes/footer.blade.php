ed1746ab5b1bdc232e9f7407efebe678a75ff655
Brayan Vilchez Daga <brayandaga5@gmail.com>
2021-01-27 19:46:26 -0500

    footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,31 +1,21 @@
 <footer class="pt-5 pb-5 mt-5 text-white bg-secondary">
     <div class="container">
-      <div class="row">
-        <div class="col-lg-5 col-xs-12 about-company">
-          <h2>Heading</h2>
-          <p class="pr-5 text-white-50">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac ante mollis quam tristique convallis </p>
-          <p><a href="#"><i class="mr-1 fa fa-facebook-square"></i></a><a href="#"><i class="fa fa-linkedin-square"></i></a></p>
+      <div class="mb-5 row">
+        <div class="col-6">
+          <img class="img-fluid" src="assets/img/perfil.png" alt="">
         </div>
-        <div class="col-lg-3 col-xs-12 links">
+        <div class="col-6 links">
           <h4 class="mt-lg-0 mt-sm-3">Links</h4>
             <ul class="p-0 m-0">
-              <li>- <a href="#"class="text-white">Lorem ipsum</a></li>
-              <li>- <a href="#"class="text-white">Nam mauris velit</a></li>
-              <li>- <a href="#"class="text-white">Etiam vitae mauris</a></li>
-              <li>- <a href="#"class="text-white">Fusce scelerisque</a></li>
-              <li>- <a href="#"class="text-white">Sed faucibus</a></li>
-              <li>- <a href="#"class="text-white">Mauris efficitur nulla</a></li>
+              <li> <a href="https://www.facebook.com/brayan.vilchezdaga" target="_blank" class="text-white"> <i class="fab fa-facebook"></i> brayan.vilchezdaga  </a></li>
+              <li> <a href="https://www.linkedin.com/in/brayan-marino-vilchez-daga-0432131b1" target="_blank" class="text-white"> <i class="fab fa-linkedin"></i> Brayan Marino Vilchez Daga  </a></li>
+              <li> <a href="https://github.com/BrayanDaga" target="_blank" class="text-white"><i class="fab fa-github"></i>BrayanDaga  </a></li>
+
             </ul>
         </div>
-        <div class="col-lg-4 col-xs-12 location">
-          <h4 class="mt-lg-0 mt-sm-4">Location</h4>
-          <p>22, Lorem ipsum dolor, consectetur adipiscing</p>
-          <p class="mb-0"><i class="mr-3 fa fa-phone"></i>(541) 754-3010</p>
-          <p><i class="mr-3 fa fa-envelope-o"></i>info@hsdf.com</p>
-        </div>
       </div>
-      <div class="text-center container-fluid bg-secondary">
-        <p class=""><small class="text-white-50">© 2019. All Rights Reserved.</small></p>
+      <div class="text-center container-fluid">
+        <p class=""><small class="text-white-50">© 2020. Brayan Marino Vilchez Daga.</small></p>
     </div>
 
 </footer>
